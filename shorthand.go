8db5a9e97b4758242b282ee37a437ed1fdf8e552
Henrique Goncalves <kamus@hadenes.io>
2024-04-17 06:44:04 -0300

    run codegen

--- shorthand.go
+++ shorthand.go
@@ -15,6 +15,7 @@ func TextToSpeechStream(streamWriter io.Writer, voiceID string, ttsReq TextToSpe
 	return getDefaultClient().TextToSpeechStream(streamWriter, voiceID, ttsReq, queries...)
 }
 
+// TextToSpeechInputStream calls the TextToSpeechInputStream method on the default client.
 func TextToSpeechInputStream(textReader io.Reader, streamWriter io.Writer, voiceID string, modelID string, ttsReq TextToSpeechInputStreamingRequest, queries ...QueryFunc) error {
 	return getDefaultClient().TextToSpeechInputStream(textReader, streamWriter, voiceID, modelID, ttsReq, queries...)
 }
